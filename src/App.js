e2d157aca0e168be10b9f24047fe1a31b7289f99
sshen <sshen@fiberhome.com>
2018-03-09 09:36:06 +0800

    initProject

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.less';
+import { Button } from 'antd';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button type="primary">Primary</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
